8f3ff5cdbe7132a8fcede42f538cee203fe661c1
bobcao3 <bobcaocheng@163.com>
2015-08-31 17:34:33 +0000

    Initial commit

---
+++ kernel/global.c
@@ -0,0 +1,24 @@
+/**
+ * proc.c
+ */
+
+#define GLOBAL_VARIABLES_HERE
+
+#include "type.h"
+#include "const.h"
+#include "protect.h"
+#include "proto.h"
+#include "proc.h"
+#include "global.h"
+
+
+PUBLIC	PROCESS			proc_table[NR_TASKS];
+
+PUBLIC	char			task_stack[STACK_SIZE_TOTAL];
+
+PUBLIC	TASK			task_table[NR_TASKS] = {{Init, STACK_SIZE_TESTA, "Init"}};
+
+PUBLIC	irq_handler		irq_table[NR_IRQ];
+
+PUBLIC	system_call		sys_call_table[NR_SYS_CALL] = {sys_get_ticks};
+
